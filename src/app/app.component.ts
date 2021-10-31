485195fe37070fd8c4344f40408b98b51a4ae9e6
stefanoVe <stefano.vecchietti.99@gmail.com>
2021-11-01 00:09:16 +0100

    heroku

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'tbc-dec-obj';
+  deactivationCode!: FormControl;
+  codiceGiusto: String = 'test';
+
+  constructor() {
+    this.deactivationCode = new FormControl('');
+  }
+
+  remaining: number = 3;
+
+  confirmCode() {
+    console.log('test', this.deactivationCode.value);
+    if (this.deactivationCode.value === this.codiceGiusto) {
+      console.log('OK');
+      return true;
+    } else {
+      this.remaining -= 1;
+      return false;
+    }
+  }
 }
